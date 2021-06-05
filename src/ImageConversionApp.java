2fe636d9c12da4b497947ccb65328caeb65c9907
Dmitry Chebykin <dim_c@mail.ru>
2021-06-05 18:06:37 +0700

    fix

--- src/ImageConversionApp.java
+++ src/ImageConversionApp.java
@@ -1,5 +1,6 @@
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
@@ -9,25 +10,30 @@ public class ImageConversionApp {
     public static final int COLORS_COUNT_IN_RGB = 3;
     public static final int MAX_COLOR_VALUE_IN_RGB = 255;
     public static final int MIN_COLOR_VALUE_IN_RGB = 0;
-    public static final int WATERCOLOR_MATRIX_MEDIAN_INDEX = 13;
-    public static final int WATERCOLOR_MATRIX_SIZE = 5;
+    public static final int SORTED_CHANNEL_COLORS_ARRAY_MEDIAN_INDEX = 13;
+    public static final int MEDIAN_BLUR_MATRIX_SIZE = 5;
 
     public static final double[][] BLUR_MATRIX = new double[][]{
-            {1 / 9.0, 1 / 9.0, 1 / 9.0},
-            {1 / 9.0, 1 / 9.0, 1 / 9.0},
-            {1 / 9.0, 1 / 9.0, 1 / 9.0}};
+            {1d / 9d, 1d / 9d, 1d / 9d},
+            {1d / 9d, 1d / 9d, 1d / 9d},
+            {1d / 9d, 1d / 9d, 1d / 9d}};
 
     public static final double[][] GAUSS_BLUR_MATRIX = new double[][]{
-            {1 / 256.0, 4 / 256.0, 6 / 256.0, 4 / 256.0, 1 / 256.0},
-            {4 / 256.0, 16 / 256.0, 24 / 256.0, 16 / 256.0, 4 / 256.0},
-            {6 / 256.0, 24 / 256.0, 36 / 256.0, 24 / 256.0, 6 / 256.0},
-            {4 / 256.0, 16 / 256.0, 24 / 256.0, 16 / 256.0, 4 / 256.0},
-            {1 / 256.0, 4 / 256.0, 6 / 256.0, 4 / 256.0, 1 / 256.0}};
+            {1d / 256d, 4d / 256d, 6d / 256d, 4d / 256d, 1d / 256d},
+            {4d / 256d, 16d / 256d, 24d / 256d, 16d / 256d, 4d / 256d},
+            {6d / 256d, 24d / 256d, 36d / 256d, 24d / 256d, 6d / 256d},
+            {4d / 256d, 16d / 256d, 24d / 256d, 16d / 256d, 4d / 256d},
+            {1d / 256d, 4d / 256d, 6d / 256d, 4d / 256d, 1d / 256d}};
 
     public static final double[][] SHARP_MATRIX = new double[][]{
             {0, -0.25, 0},
             {-0.25, 2, -0.25},
             {0, -0.25, 0}};
+
+    public static final double[][] OUTLINE_MATRIX = new double[][]{
+            {-0.5, -0.5, -0.5},
+            {-0.5, 5, -0.5},
+            {-0.5, -0.5, -0.5}};
 
     public static void main(String[] args) {
         File file = new File("image.jpg");
@@ -40,47 +46,38 @@ public class ImageConversionApp {
             return;
         }
 
-        WritableRaster raster = source.getRaster();
+        WritableRaster sourceRaster = source.getRaster();
+        ColorModel colorModel = source.getColorModel();
 
-        // Create new images of the same size
-        BufferedImage blurImage = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
-        WritableRaster blurRaster = blurImage.getRaster();
-        convertRaster(raster, blurRaster, BLUR_MATRIX);
-        File output1 = new File("image_blur.jpg");
+        WritableRaster blurRaster = getConvertedRaster(sourceRaster, BLUR_MATRIX);
+        writeRasterToImageFile(blurRaster, colorModel, "image_blur.jpg", "jpg");
 
-        BufferedImage gaussBlurImage = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
-        WritableRaster gaussBlurRaster = gaussBlurImage.getRaster();
-        convertRaster(raster, gaussBlurRaster, GAUSS_BLUR_MATRIX);
-        File output2 = new File("image_sharp.jpg");
+        WritableRaster gaussBlurRaster = getConvertedRaster(sourceRaster, GAUSS_BLUR_MATRIX);
+        writeRasterToImageFile(gaussBlurRaster, colorModel, "image_gauss_blur.jpg", "jpg");
 
-        BufferedImage sharpImage = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
-        WritableRaster sharpRaster = sharpImage.getRaster();
-        convertRaster(raster, sharpRaster, SHARP_MATRIX);
-        File output3 = new File("image_gauss_blur.jpg");
+        WritableRaster sharpRaster = getConvertedRaster(sourceRaster, SHARP_MATRIX);
+        writeRasterToImageFile(sharpRaster, colorModel, "image_sharp.jpg", "jpg");
 
-        BufferedImage watercolorImage = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
-        WritableRaster watercolorRaster = watercolorImage.getRaster();
-        convertRaster(gaussBlurRaster, watercolorRaster);
-        File output4 = new File("image_watercolor.jpg");
+        WritableRaster outlineRaster = getConvertedRaster(sourceRaster, OUTLINE_MATRIX);
+        writeRasterToImageFile(outlineRaster, colorModel, "image_outline.jpg", "jpg");
 
-        try {
-            ImageIO.write(blurImage, "jpg", output1);
-            ImageIO.write(sharpImage, "jpg", output2);
-            ImageIO.write(gaussBlurImage, "jpg", output3);
-            ImageIO.write(watercolorImage, "jpg", output4);
-        } catch (IOException e) {
-            System.out.println("Файлы не записываются.");
-        }
+        WritableRaster medianBlurRaster = getConvertedRaster(gaussBlurRaster);
+        writeRasterToImageFile(medianBlurRaster, colorModel, "image_median_blur.jpg", "jpg");
+
+        WritableRaster watercolorRaster = getConvertedRaster(medianBlurRaster, OUTLINE_MATRIX);
+        writeRasterToImageFile(watercolorRaster, colorModel, "image_watercolor.png", "png");
     }
 
-    private static void convertRaster(WritableRaster sourceRaster, WritableRaster destinationRaster, double[][] matrix) {
+    private static WritableRaster getConvertedRaster(WritableRaster sourceRaster, double[][] matrix) {
         int rasterHeight = sourceRaster.getHeight();
         int rasterWidth = sourceRaster.getWidth();
         int matrixSize = matrix.length;
 
+        WritableRaster destinationRaster = sourceRaster.createCompatibleWritableRaster();
+
         if (rasterHeight < matrixSize || rasterWidth < matrixSize) {
             destinationRaster.setDataElements(0, 0, sourceRaster);
-            return; // the image does not meet the minimum size for transformations, just copy raster
+            return destinationRaster; // the image does not meet the minimum size for transformations, just copy raster
         }
 
         int leftIndexMargin = -matrixSize / 2;
@@ -94,7 +91,6 @@ public class ImageConversionApp {
 
                 for (int i = leftIndexMargin; i <= rightIndexMargin; i++) {
                     for (int j = leftIndexMargin; j <= rightIndexMargin; j++) {
-
                         int pixelByMatrixCoordinateX = x + j;
                         int pixelByMatrixCoordinateY = y + i;
 
@@ -121,36 +117,29 @@ public class ImageConversionApp {
                 destinationRaster.setPixel(x, y, colors);
             }
         }
+
+        return destinationRaster;
     }
 
-    private static double getNormalizedColor(double value) {
-        if (value < MIN_COLOR_VALUE_IN_RGB) {
-            return MIN_COLOR_VALUE_IN_RGB;
-        }
-        if (value > MAX_COLOR_VALUE_IN_RGB) {
-            return MAX_COLOR_VALUE_IN_RGB;
-        }
-
-        return value;
-    }
-
-    private static void convertRaster(WritableRaster sourceRaster, WritableRaster destinationRaster) {
+    private static WritableRaster getConvertedRaster(WritableRaster sourceRaster) {
         int rasterHeight = sourceRaster.getHeight();
         int rasterWidth = sourceRaster.getWidth();
 
-        if (rasterHeight < WATERCOLOR_MATRIX_SIZE || rasterWidth < WATERCOLOR_MATRIX_SIZE) {
-            destinationRaster.setDataElements(0, 0, sourceRaster); // the image does not meet the minimum size for transformations, just copy raster
-            return;
+        WritableRaster destinationRaster = sourceRaster.createCompatibleWritableRaster();
+
+        if (rasterHeight < MEDIAN_BLUR_MATRIX_SIZE || rasterWidth < MEDIAN_BLUR_MATRIX_SIZE) {
+            destinationRaster.setDataElements(0, 0, sourceRaster);
+            return destinationRaster; // the image does not meet the minimum size for transformations, just copy raster
         }
 
-        int leftIndexMargin = -WATERCOLOR_MATRIX_SIZE / 2;
-        int rightIndexMargin = WATERCOLOR_MATRIX_SIZE / 2;
+        int leftIndexMargin = -MEDIAN_BLUR_MATRIX_SIZE / 2;
+        int rightIndexMargin = MEDIAN_BLUR_MATRIX_SIZE / 2;
 
         int[] colors = new int[COLORS_COUNT_IN_RGB];
 
-        int[] r = new int[WATERCOLOR_MATRIX_SIZE * WATERCOLOR_MATRIX_SIZE];
-        int[] g = new int[WATERCOLOR_MATRIX_SIZE * WATERCOLOR_MATRIX_SIZE];
-        int[] b = new int[WATERCOLOR_MATRIX_SIZE * WATERCOLOR_MATRIX_SIZE];
+        int[] r = new int[MEDIAN_BLUR_MATRIX_SIZE * MEDIAN_BLUR_MATRIX_SIZE];
+        int[] g = new int[MEDIAN_BLUR_MATRIX_SIZE * MEDIAN_BLUR_MATRIX_SIZE];
+        int[] b = new int[MEDIAN_BLUR_MATRIX_SIZE * MEDIAN_BLUR_MATRIX_SIZE];
 
         for (int y = 0; y < rasterHeight; y++) {
             for (int x = 0; x < rasterWidth; x++) {
@@ -181,16 +170,37 @@ public class ImageConversionApp {
                 }
 
                 Arrays.sort(r);
-                colors[0] = r[WATERCOLOR_MATRIX_MEDIAN_INDEX];
+                colors[0] = r[SORTED_CHANNEL_COLORS_ARRAY_MEDIAN_INDEX];
 
                 Arrays.sort(g);
-                colors[1] = g[WATERCOLOR_MATRIX_MEDIAN_INDEX];
+                colors[1] = g[SORTED_CHANNEL_COLORS_ARRAY_MEDIAN_INDEX];
 
                 Arrays.sort(b);
-                colors[2] = b[WATERCOLOR_MATRIX_MEDIAN_INDEX];
+                colors[2] = b[SORTED_CHANNEL_COLORS_ARRAY_MEDIAN_INDEX];
 
                 destinationRaster.setPixel(x, y, colors);
             }
+        }
+
+        return destinationRaster;
+    }
+
+    private static double getNormalizedColor(double value) {
+        if (value < MIN_COLOR_VALUE_IN_RGB) {
+            return MIN_COLOR_VALUE_IN_RGB;
+        }
+        if (value > MAX_COLOR_VALUE_IN_RGB) {
+            return MAX_COLOR_VALUE_IN_RGB;
+        }
+
+        return value;
+    }
+
+    private static void writeRasterToImageFile(WritableRaster raster, ColorModel colorModel, String path, String format) {
+        try {
+            ImageIO.write(new BufferedImage(colorModel, raster, colorModel.isAlphaPremultiplied(), null), format, new File(path));
+        } catch (IOException e) {
+            System.out.println("Файл " + path + " не записываются.");
         }
     }
 }
